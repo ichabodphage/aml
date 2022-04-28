0788e11571ea56cd000df36a754cbaa58b03c36a
ichabodphage <agar.ioisgud@gmail.com>
2022-04-28 11:15:24 -0500

    documented lib.hpp

--- include/lib.hpp
+++ include/lib.hpp
@@ -16,36 +16,37 @@ namespace aml
 {
     /**
      * @brief gets the Nth element in a template parameter pack
-     * 
+     *
      * @tparam N template element
      * @tparam Ts template parameter pack
      */
     template <int N, typename... Ts>
     using indexInTemplate =
         typename std::tuple_element<N, std::tuple<Ts...>>::type;
-    
-    
+
     /**
      * @brief returns the combined sizeof every item in a template parameter pack
-     * 
+     *
      * @tparam T template parameter pack
      * @return constexpr size_t sum of the size of all the parameters
      */
     template <typename... T>
     constexpr size_t packSize()
     {
-        
+
         return (sizeof(T) + ...);
     }
 
-    template<typename T, typename... Ts>
+    template <typename T, typename... Ts>
     constexpr bool contains()
-    { return std::disjunction_v<std::is_same<T, Ts>...>; }
+    {
+        return std::disjunction_v<std::is_same<T, Ts>...>;
+    }
 
     /**
      * @brief checks for any openGL errors, throws a runtime error if any errors are caught
-     * 
-     * 
+     *
+     *
      * @param file path to the file to check errors (should be _FILE_)
      * @param line line to alert error from (should be _LINE_)
      */
@@ -53,33 +54,35 @@ namespace aml
 
     /**
      * @brief starts aml within the program on a global scale
-     * 
+     *
      * @param antialiasingLevel level of antialiasing, defaults to 1
      * @param resize flag for if windows should be resizable or not, defaults to false
      */
     void startAml(int antialiasingLevel = 1, bool resize = false);
 
     /**
-     * @brief interweaves 2 standard vectors holding 3 dimenstional vectors into a single vertex array
-     * 
-     * @details 
+     * @brief template function that interweaves 2 standard vectors holding vectors into a single vertex array
+     *
+     * @details
      * used to provide compatibility between arrays of raw vectors and arrays of verticies
-     * 
+     *
+     *
      * @param posArray array of position vectors
      * @param colorArray array of color vectors
-     * @return std::vector<aml::Vert3> standard vector of 3d vertices
+     * @return std::vector<vertType> standard vector of vertTyoe vertices
      */
-    std::vector<aml::Vert3> makeVertexArr(std::vector<glm::vec3> &posArray,std::vector<glm::vec3> &colorArray);
+    
+    std::vector<aml::Vert3> makeVertexArr(std::vector<glm::vec3> &posArray, std::vector<glm::vec3> &colorArray);
 
     /**
-     * @brief terminates AML 
-     * 
+     * @brief terminates AML
+     *
      */
     void stopAml();
 
     /**
      * @brief creates a raw openGL window
-     * 
+     *
      * @param width width of the window
      * @param height height of the window
      * @param name display name of the window
