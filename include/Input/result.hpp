9409ca2b9646f3e9471351141b1c3c918185c8bd
ichabodphage <agar.ioisgud@gmail.com>
2022-05-06 18:09:24 -0500

    updated result.hpp

--- include/Input/result.hpp
+++ include/Input/result.hpp
@@ -3,21 +3,37 @@
 #include <GL/glut.h>
 #include <glm/glm.hpp>
 #include <GLFW/glfw3.h>
-
+#include "inputType.hpp"
 #ifndef AML_RESULT
 #define AML_RESULT
 
 namespace aml{
-    /*
-        struct that holds the result of polling an input
-    */
-   struct Result{
-       //state of the input
-       int inputState;
-       //activity of input
-       bool active;
-       //position of input
-       glm::vec2 position;
+    /**
+     * @brief class that represents the result of an input
+     * 
+     */
+    class Result{
+        
+        /// @brief type of the input
+        aml::InputType type;
+        
+        /// @brief activity of input
+        bool active;
+        
+        /**
+         * @brief union that holds relavant information per type of input
+         * 
+         */
+        union {
+            /// @brief keycode of a keyboard input
+            int key;
+            /// @brief magnitude of a scroll input
+            glm::vec2 magnitude;
+            /// @brief position of a mouse click or move input
+            glm::vec2 pos;
+        } state;
+
+        
    };
 
 }
